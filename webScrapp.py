3a4305ad1c2a4968c80bd522eff1c18f45fa2d90
vicente322 <vicentehofmeister@gmail.com>
2024-03-22 08:54:10 -0300

    avanco webScrapp com import para csv

--- webScrapp.py
+++ webScrapp.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import csv
 
 link_list = []
 html_list = []
@@ -24,11 +25,28 @@ def baixa_pagina(link):
       html_list.append(BeautifulSoup(linksoup.read(), 'html.parser'))
       return BeautifulSoup(linksoup.read(), 'html.parser')
 
+def salva_dados(souphtml):
+      for tag in souphtml.find_all('tr'):
+            if 'id="places_country_row"' in tag.attrs:
+                  print(tag.attrs)
+            print(tag)
+
 html = urlopen('http://127.0.0.1:8000/places/default/index')
 
 busca_links(html)
 for link in link_list:
       baixa_pagina(link)
 
-for link in link_list:
-      print(link)
+# with open ('paises.csv', newline='') as csvfile:
+#       leitor = csv.reader(csvfile, delimiter='	', quotechar='|')
+#       for linha in leitor:
+#             print(', '.join(linha))
+
+# with open ('paises.csv', 'w', newline='') as csvfile:
+#       escritor = csv.writer(csvfile, delimiter='	', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#       escritor.writerow([12345] * 5)
+
+salva_dados(html_list.pop(0))
+
+# for link in link_list:
+      # print(link)
