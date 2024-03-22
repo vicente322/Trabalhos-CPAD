9facbc86c5d0288f38dfdf3232251d5ecb83a3d1
vicente322 <vicentehofmeister@gmail.com>
2024-03-22 11:43:49 -0300

    webScrapp realizando downloado e import para csv

--- webScrapp.py
+++ webScrapp.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import csv
+import time
 
 link_list = []
 html_list = []
@@ -25,28 +26,49 @@ def baixa_pagina(link):
       html_list.append(BeautifulSoup(linksoup.read(), 'html.parser'))
       return BeautifulSoup(linksoup.read(), 'html.parser')
 
-def salva_dados(souphtml):
-      for tag in souphtml.find_all('tr'):
-            if 'id="places_country_row"' in tag.attrs:
-                  print(tag.attrs)
-            print(tag)
+def salva_dados():
+      with open ('paises.csv', 'w', newline='') as csvfile:
+            escritor = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            escritor.writerow(['Pais','Capital', 'Area', 'Paises vizinhos'])
 
+            for html in html_list:
+
+                  nome = ''
+                  capital = ''
+                  area = 0
+                  vizinhos = ''
+                  timestamp = 0
+
+                  for tag in html.find_all('td', class_="w2p_fw"):
+                        if tag.previous_element == "Country: ":
+                              nome = tag.string
+                        
+                        if tag.previous_element == "Capital: ":
+                              capital = tag.string
+                        
+                        if tag.previous_element == "Area: ":
+                              area = int(tag.string.replace(' square kilometres', ''))
+
+                        if tag.previous_element == "Neighbours: ":
+                              for link in tag.find_all('a'):
+                                    if link['href'] != '/places/default/iso//':
+                                          linksoup = urlopen('http://127.0.0.1:8000' + link['href'])
+                                          vizinho = BeautifulSoup(linksoup.read(), 'html.parser')
+                                          for tagVizinho in vizinho.find_all('td', class_="w2p_fw"):
+                                                if tagVizinho.previous_element == "Country: ":
+                                                      if vizinhos != '':
+                                                            vizinhos += ', '
+                                                      vizinhos += tagVizinho.string                            
+
+                        timestamp = time.time()
+
+                  escritor.writerow([nome, capital, area, vizinhos, timestamp])
+
+            
 html = urlopen('http://127.0.0.1:8000/places/default/index')
 
 busca_links(html)
 for link in link_list:
       baixa_pagina(link)
 
-# with open ('paises.csv', newline='') as csvfile:
-#       leitor = csv.reader(csvfile, delimiter='	', quotechar='|')
-#       for linha in leitor:
-#             print(', '.join(linha))
-
-# with open ('paises.csv', 'w', newline='') as csvfile:
-#       escritor = csv.writer(csvfile, delimiter='	', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#       escritor.writerow([12345] * 5)
-
-salva_dados(html_list.pop(0))
-
-# for link in link_list:
-      # print(link)
+salva_dados()
