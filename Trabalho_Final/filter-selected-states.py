b5b1caa6382402a05e090e2ebbd58b2be25f1e69
alexiadorneles <alexiadorneles02@gmail.com>
2024-06-28 21:07:44 -0300

    fix filter script

--- Trabalho_Final/filter-selected-states.py
+++ Trabalho_Final/filter-selected-states.py
@@ -4,13 +4,21 @@ import os
 # Lista de estados que queremos, índices de acordo com tabela do IBGE: https://www.ibge.gov.br/explica/codigos-dos-municipios.php
 # 43 = Rio Grande do Sul
 # 53 = Distrito Federal
-sg_uf_list = [43, 53]
+sg_uf_list = [32, 35, 43, 52]
 
 def filter_data_by_ufs(file_name):
     output_dir = './Data/Filtered_CSV'
     os.makedirs(output_dir, exist_ok=True)
     df = pd.read_csv("./Data/CSV/" + file_name, sep=';', low_memory=False)
+    
+    df['SG_UF'] = pd.to_numeric(df['SG_UF'].str.strip(), errors='coerce')
+    # Drop rows with NaN values in SG_UF
+    df = df.dropna(subset=['SG_UF'])
+    # Convert SG_UF to integers
+    df['SG_UF'] = df['SG_UF'].astype(int)
+    # Filter the DataFrame
     df_filtered = df[df['SG_UF'].isin(sg_uf_list)]
+
     df_filtered.to_csv("./Data/Filtered_CSV/" + file_name, index=False, sep=';')
     print("Filtered data saved back to", "./Data/Filtered_CSV/" + file_name)
 
